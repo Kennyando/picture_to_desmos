185f61148dfa37c1582bcac16a22f0f21d0d2399
Kennan Neo <kneokd@yahoo.com>
2024-03-30 18:23:22 +0800

    outline

---
+++ app.py
@@ -0,0 +1,9 @@
+#find a way to get a input of a picture
+#read the information of the picture
+# find the main features of the picture (maybe start with face first)
+    #use an ai to recognize the main features
+#convert the ai to meaningful results ( convert it to graph equations)
+    #what kind of graphs?
+#find a way to put the graph outputs in desmos automatically
+    #ideally itll redirect with a link to a desmos website
+
